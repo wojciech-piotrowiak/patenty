b4bed939382d48846effd91c1a848fb9bf6de36c
wojtek <wojciech.piotrowiak@gmail.com>
2017-11-18 20:56:52 +0100

    Patenty nr.2

--- src/Patenty2.java
+++ src/Patenty2.java
@@ -1,12 +1,25 @@
+import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Patenty2 {
 
     @Test
+    public void memoryCheck() {
+        System.out.println(ObjectSizeCalculator.getObjectSize(null));
+        System.out.println(ObjectSizeCalculator.getObjectSize(new Object()));
+        System.out.println(ObjectSizeCalculator.getObjectSize("ABCD"));
+        System.out.println(ObjectSizeCalculator.getObjectSize(true));
+        System.out.println(ObjectSizeCalculator.getObjectSize(3));
+        System.out.println(ObjectSizeCalculator.getObjectSize('A'));
+    }
+
+    @Test
     public void joiningTest() {
-        System.out.print(Stream.of("1", "2", "3").collect(Collectors.joining("<")));
+        assertEquals("1<2<3",Stream.of("1", "2", "3").collect(Collectors.joining("<")));
     }
 }
